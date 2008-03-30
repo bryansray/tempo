a2991cbfdf70146a7b1591bebaf4744d906c94e3
Bryan Ray <bryansray@gmail.com>
2008-03-29 21:51:56 -0500

    Adding in Capistrano deployment files.
    These should work, but will need to be customized based on server addresses.s

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,27 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "Tempo"
+set :repository,  "git@github.com:bryansray/tempo.git"
+set :domain, "web-server"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+default_run_options[:pty] = true
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
+set :scm, :git
+set :deploy_via, :remote_cache
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+ssh_options[:paranoid] = false
+
+set :user, "mongrel"
+set :runner, "mongrel"
+set :use_sudo, false
+
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+# Move over configuration files after deploying the code
+task :update_config, :roles => [:app] do
+  run "cp -Rf #{shared_path}/config/* #{release_path}/config/"
+end
+
+after 'deploy:update_code', :update_config
