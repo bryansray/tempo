e372c114b1de8146147d02b211130dbef07abb9f
Bryan Ray <bryansray@gmail.com>
2008-04-14 23:44:40 -0500

    Fixed up the fixture data to be more accurate and a tabbing issue (anal)

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,7 @@ class Blog < ActiveRecord::Base
 	belongs_to :user
 	
 	has_many :posts, :order => "posts.created_at"
-        has_many :published_posts, :class_name => "Post", :include => :content, :conditions => ["contents.published = ?", true], :order => "posts.created_at DESC"
+  has_many :published_posts, :class_name => "Post", :include => :content, :conditions => ["contents.published = ?", true], :order => "posts.created_at DESC"
 	
 	# Validations
 	validates_presence_of :name
