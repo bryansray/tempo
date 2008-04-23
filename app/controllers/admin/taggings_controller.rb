c2423ce047d33c5394d8e7e92514362d8cee2c8e
Bryan Ray <bryansray@gmail.com>
2008-04-22 23:49:09 -0500

    Refactored _applicator to use a taggable_type hidden field
    * Created a TaggingsController that will determine which object needs to have tags added to it via that [:tags][:taggable_type]. I'm pretty sure I like this, but still need to work on it.
    * The applicator passes a hidden field taggable_type to the TaggingsController
    
    This is first pass ... still needs some work.

--- app/controllers/admin/taggings_controller.rb
+++ app/controllers/admin/taggings_controller.rb
@@ -3,4 +3,30 @@ class Admin::TaggingsController < ApplicationController
     @tagging = Tagging.find params[:id]
     @tagging.destroy
   end
+  
+  def update
+    puts "--- #{params.inspect}"
+    type = params[:tags][:taggable_type].constantize
+    @content =  type.find params[:id]
+    @content.tag_list.add params[:tags][:tag_list], :parse => true
+    
+    respond_to do |format|
+      if @content.save
+        format.js
+      end
+    end    
+  end
+  
+  def create
+    puts "--- #{params.inspect}"
+    type = params[:tags][:taggable_type].constantize
+    @content =  type.find params[:id]
+    @content.tag_list.add params[:tags][:tag_list], :parse => true
+    
+    respond_to do |format|
+      if @content.save
+        format.js
+      end
+    end
+  end
 end
--- spec/controllers/admin/taggings_controller_spec.rb
+++ spec/controllers/admin/taggings_controller_spec.rb
@@ -27,3 +27,37 @@ describe Admin::TaggingsController, "handling DELETE /admin/taggings/1" do
     do_delete
   end
 end
+
+describe Admin::TaggingsController, "handling POST /admin/taggings" do
+  before(:each) do
+    @content = mock_model(Content, :to_param => 1)    
+    @tag_list = mock_model(TagList)
+    Content.stub!(:find).and_return(@content)
+    @content.stub!(:tag_list).and_return(@tag_list)
+    @tag_list.stub!(:add).and_return(@tag_list)
+  end
+  
+  def do_post
+    post :create, :id => 1, :tags => { :taggable_type => 'Content', :tag_list => "Tag 1" }
+  end
+  
+  def post_with_successful_save
+    @content.should_receive(:save).and_return(true)
+    do_post
+  end
+  
+  it "should be successful" do
+    post_with_successful_save
+    response.should be_success
+  end
+  
+  it "should find the taggable type" do
+    Content.should_receive(:find).with('1').and_return(@content)
+    post_with_successful_save
+  end
+  
+  it "should add the new tags to the taggable type" do
+    @tag_list.should_receive(:add).with("Tag 1", :parse => true).and_return(@tag_list)
+    post_with_successful_save
+  end
+end
